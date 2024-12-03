67b79e80bfc0373f1580882329d98aa85641336e
Steve Hunneyball <stevehunneyball@gmail.com>
2024-12-03 13:11:47 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+/*
+    this needs to...
+    connect to nats
+    subscribe to a subject
+    queue group
+
+ */
+import 'dotenv/config';
+import {Server} from "./nats/server";
+
+async function main() {
+    console.log('Hello, world!');
+    const server = new Server();
+    await server.establishConnection();
+    // then run a process that gets the messages off of the queue
+    await server.subscribe("potato.masher");
+}
+
+main().then(() => console.log('Goodbye, world!'));
